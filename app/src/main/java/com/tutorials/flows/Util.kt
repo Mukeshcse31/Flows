08ccdb79cb12ddaae1bd19be6f49861f33864bfd
Mukesh Raja J <Mukeshcse31@gmail.com>
2024-05-27 18:25:58 -0400

    qraphql apis

--- app/src/main/java/com/tutorials/flows/Util.kt
+++ app/src/main/java/com/tutorials/flows/Util.kt
@@ -1,15 +1,23 @@
 package com.tutorials.flows
 
+import com.google.gson.Gson
 import com.tutorials.flows.service.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Util {
+//    Gson()
 
-    val retrofit = Retrofit.Builder()
-    .baseUrl(Constants.BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create())
-    .build()
+    val gson = Gson().newBuilder().setDateFormat("YYYYMM").create()
 
-    val apiService: ApiService = retrofit.create(ApiService::class.java)
+    private fun getRetrofit(url: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    fun getService(url: String): ApiService {
+       return getRetrofit(url).create(ApiService::class.java)
+    }
 }
--- app/src/main/java/com/tutorials/flows/viewmodel/GraphQlViewModel.kt
+++ app/src/main/java/com/tutorials/flows/viewmodel/GraphQlViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tutorials.flows.model.GraphQlRequest
 import com.tutorials.flows.model.GraphQlResponse
+import com.tutorials.flows.model.GraphResponseWithParams
 import com.tutorials.flows.service.ApiHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,28 +13,27 @@ import kotlinx.coroutines.launch
 
 //https://stackoverflow.com/questions/40875978/how-to-use-graphql-with-retrofit-on-android
 // https://amitshekhar.me/blog/retrofit-with-kotlin-flow
-class GraphQlViewModel(val apiHelper: ApiHelper) : ViewModel() {
-
-//    private val _data: MutableLiveData<GraphQlResponse> = MutableLiveData(null)
-//    val data: LiveData<GraphQlResponse?> = _data
+class GraphQlViewModel(private val apiHelper: ApiHelper) : ViewModel() {
 
     val state = MutableStateFlow<GraphQlResponse?>(null)
+
+    val state1 = MutableStateFlow<GraphResponseWithParams?>(null)
 
     fun getGraphRes() {
         viewModelScope.launch {
             val body = GraphQlRequest(
-                "query Query {\n" +
-                        "  country(code: \"BR\") {\n" +
-                        "    name\n" +
-                        "    native\n" +
-                        "    capital\n" +
-                        "    emoji\n" +
-                        "    currency\n" +
-                        "    languages {\n" +
-                        "      code\n" +
-                        "      name\n" +
-                        "    }\n" +
-                        "  }\n" +
+                "query Query {" +
+                        "  country(code: \"BR\") {" +
+                        "    name" +
+                        "    native" +
+                        "    capital" +
+                        "    emoji" +
+                        "    currency" +
+                        "    languages {" +
+                        "      code" +
+                        "      name" +
+                        "    }" +
+                        "  }" +
                         "}",
                 emptyMap()
             )
@@ -41,6 +41,55 @@ class GraphQlViewModel(val apiHelper: ApiHelper) : ViewModel() {
                 .flowOn(Dispatchers.IO)
                 .collect {
                     state.value = it
+                    println("GraphQl response viewmodel: $it")
+                }
+        }
+    }
+
+    /*
+    Backpressure Handling
+    Cold Streams
+    More Powerful Operators:
+
+    Rich Set of Operators: Flow provides a comprehensive set of operators for transformations (map, filter, flatMap),
+    combining flows (merge, zip), and handling errors (catch, retry), offering greater flexibility and control.
+
+    Error Handling:
+
+    Built-In Error Handling: Flow has built-in mechanisms for handling errors gracefully using operators like catch and retry,
+    providing robust error management capabilities.
+
+    Reactive Streams Compliance
+
+    Lifecycle Awareness:
+
+    Lifecycle Scoping: While LiveData is lifecycle-aware out of the box,
+    Flow can achieve similar behavior using extensions like lifecycleScope and repeatOnLifecycle.
+
+     */
+
+    fun getGraphWithParamsRes() {
+        viewModelScope.launch {
+//            val map1 = mapOf("characterId" to 11)
+            val body = GraphQlRequest(
+
+//                "query ExampleQuery(characterId: ID!, page: Int) {" +
+//                        "character(id: \$characterId) {" +
+                "query ExampleQuery {" +
+                        " character(id: 31) {" +
+                        " created" +
+                        " species" +
+                        " status" +
+                        " type" +
+                        " }" +
+                        "}",
+//                emptyMap()
+                mapOf("characterId" to 11, "page" to null)
+            )
+            apiHelper.getGraphWithParamsRes(body)
+                .flowOn(Dispatchers.IO)
+                .collect {
+                    state1.value = it
                     println("GraphQl response viewmodel: $it")
                 }
         }
